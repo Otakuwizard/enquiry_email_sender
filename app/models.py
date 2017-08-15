d6ea94e60ccc4327d9e76c229f634f7f4fdacfbc
Zyklop <fzj890909@gmail.com>
2017-08-15 17:46:35 +0200

    Models develop

--- app/models.py
+++ app/models.py
@@ -9,21 +9,28 @@ def generate_id():
 class Enquiry(db.Model):
     __tablename__ = 'enquiries'
     id = db.Column(db.String(64), primary_key=True, default=generate_id)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    asker = db.Column(db.String(32), nullable=False)
+    enquiry_number = db.Column(db.String(64), index=True, unique=True, nullable=False)
+    recipient = db.Column(db.String(64))
+    quotes = db.relationship('Quote', backref='enquiry', lazy='dynamic')
+    enquiry_table = db.relationship('EnquiryTable', backref='enquiry', lazy='dynamic')
+
+class EnquiryTable(db.Model):
+    __tablename__ = 'enquiry_tables'
+    id = db.Column(db.String(64), primary_key=True, default=generate_id)
     created_at = db.Column(db.DateTime(), default=datetime.utcnow)
-    enquiry_number = db.Column(db.String(64), index=True, unique=True)
     brand = db.Column(db.String(32), index=True)
     typ_number = db.Column(db.String(64))
     amount = db.Column(db.Integer)
-    recipient = db.Column(db.String(64))
-    asker = db.Column(db.String(64))
-    quotes = db.relationship('Quote', backref='enquiry', lazy='dynamic')
+    enquiry_id = db.Column(db.String(64), db.ForeignKey('enquiries.enquiry_number'))
 
 class Quote(db.Model):
     __tablename__ = 'quotes'
     id = db.Column(db.String(64), primary_key=True, defualt=generate_id)
     created_at = db.Column(db.DateTime(), default=datetime.utcnow)
     quote_number = db.Column(db.String(64), index=True)
-    enquiry_id = db.Column(db.String(64), db.ForeignKey('enquiries.number'))
+    enquiry_id = db.Column(db.String(64), db.ForeignKey('enquiries.enquiry_number'))
     series_number = db.Column(db.String(64), index=True)
     discount = db.Column(db.Float)
     unit_price = db.Column(db.Float)
@@ -33,4 +40,5 @@ class Quote(db.Model):
     delivery_time = db.Column(db.String(32))
     weight = db.Column(db.Float)
 
-
+class QuoteTable(db.Model):
+    __tablename__ =  'quote_tables'
